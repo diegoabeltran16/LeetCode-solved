e6a4d9e253a1d7ece11a637301fef1b8ceba21d8
Diego Alejandro Beltran <159483287+diegoabeltran16@users.noreply.github.com>
2025-06-28 11:46:56 -0500

    workflow codes done

--- problems/13_roman_to_Integer/roman_to_integer.java
+++ problems/13_roman_to_Integer/roman_to_integer.java
@@ -1,3 +1,6 @@
+import java.util.Map;
+import java.util.HashMap;
+
 class Solution {
     public int romanToInt(String s) {
         Map<Character, Integer> romanMap = new HashMap<>();
@@ -9,19 +12,15 @@ class Solution {
         romanMap.put('D', 500);
         romanMap.put('M', 1000); 
         int r=0,pv=1; 
-        for(int i=s.length()-1;i>=0;i--) 
-            { 
-                int cv= romanMap.get(s.charAt(i));
-                if(cv<pv) 
-                {
-                    r-=cv; 
-                }
-                else
-                {
-                    r+=cv; 
-                }
-                pv=cv; 
-            } 
+        for(int i=s.length()-1;i>=0;i--) { 
+            int cv= romanMap.get(s.charAt(i));
+            if(cv<pv) {
+                r-=cv; 
+            } else {
+                r+=cv; 
+            }
+            pv=cv; 
+        } 
         return r;
     } 
 }
