07bdeabeb27c06faa7749857c54b47ed994b114f
Diego Alejandro Beltran <159483287+diegoabeltran16@users.noreply.github.com>
2025-06-28 11:51:15 -0500

    update roman to integer java

--- problems/13_roman_to_Integer/roman_to_integer.java
+++ problems/13_roman_to_Integer/roman_to_integer.java
@@ -1,8 +1,22 @@
 import java.util.Map;
 import java.util.HashMap;
 
+/**
+ * Solution for LeetCode Problem 13: Roman to Integer.
+ *
+ * This class provides a method to convert a Roman numeral string into its integer value.
+ * The conversion works from right to left, subtracting values when a smaller numeral precedes a larger one,
+ * and adding otherwise, according to Roman numeral rules.
+ */
 class Solution {
+    /**
+     * Converts a Roman numeral string to its integer representation.
+     *
+     * @param s the Roman numeral string (e.g., "MCMXCIV")
+     * @return the integer value of the Roman numeral
+     */
     public int romanToInt(String s) {
+        // Map each Roman numeral to its integer value
         Map<Character, Integer> romanMap = new HashMap<>();
         romanMap.put('I', 1);
         romanMap.put('V', 5);
@@ -11,16 +25,22 @@ class Solution {
         romanMap.put('C', 100);
         romanMap.put('D', 500);
         romanMap.put('M', 1000); 
-        int r=0,pv=1; 
-        for(int i=s.length()-1;i>=0;i--) { 
-            int cv= romanMap.get(s.charAt(i));
-            if(cv<pv) {
-                r-=cv; 
+
+        int result = 0;
+        int prevValue = 1; // Previous numeral value (start with 1 for the rightmost character)
+
+        // Iterate from right to left through the string
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currValue = romanMap.get(s.charAt(i));
+            if (currValue < prevValue) {
+                // If current value is less than previous, subtract it
+                result -= currValue; 
             } else {
-                r+=cv; 
+                // Otherwise, add it
+                result += currValue; 
             }
-            pv=cv; 
+            prevValue = currValue; 
         } 
-        return r;
+        return result;
     } 
 }
