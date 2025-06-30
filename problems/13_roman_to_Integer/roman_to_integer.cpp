564ab2c94df2778a67d13ebc3721a8bff0e40969
Diego Alejandro Beltran <159483287+diegoabeltran16@users.noreply.github.com>
2025-06-30 11:27:44 -0400

    Update roman_to_integer.cpp

--- problems/13_roman_to_Integer/roman_to_integer.cpp
+++ problems/13_roman_to_Integer/roman_to_integer.cpp
@@ -1,73 +1,61 @@
+/**
+ * LeetCode Problem 13: Roman to Integer
+ *
+ * This class provides a method to convert a Roman numeral string
+ * into its corresponding integer value.
+ *
+ * The implementation handles standard Roman numeral notation,
+ * including subtractive combinations (e.g., IV for 4, IX for 9, etc.).
+ *
+ * Example usage:
+ *   Solution sol;
+ *   int result = sol.romanToInt("MCMXCIV"); // result = 1994
+ */
+
+#include <string>
+
 class Solution {
 public:
+    /**
+     * Converts a Roman numeral string to its integer representation.
+     *
+     * @param s A string containing the Roman numeral (e.g., "MCMXCIV").
+     * @return The integer value corresponding to the Roman numeral.
+     */
     int romanToInt(std::string s)
     {
-
         int sum {0};
 
-        //std::unordered_map<char, int> romanToValue{{'I',1}, {'V',5}, {'X',10}, {'L',50}, {'C',100}, {'D',500}, {'M',1000}};
-
+        // Lambda function to return the integer value of a Roman numeral character.
         auto romanToValue = [](char roman) constexpr {
             switch (roman)
             {
-                case 'I':
-                {   
-                    return 1;
-                    break;
-                }
-                case 'V':
-                {
-                    return 5;
-                    break;
-                }
-                case 'X':
-                {
-                    return 10;
-                    break;
-                }
-                case 'L':
-                {
-                    return 50;
-                    break;
-                }
-                case 'C':
-                {
-                    return 100;
-                    break;
-                }
-                case 'D':
-                {
-                    return 500;
-                    break;
-                }
-                case 'M':
-                {
-                    return 1000;
-                    break;
-                }
-                default:
-                {
-                    return 0;
-                    break;
-                }
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: return 0;
             }
         };
 
-        for (int i=0; i<s.length(); ++i)
+        // Iterate through the string, adding or subtracting values based on Roman numeral rules.
+        for (int i = 0; i < s.length(); ++i)
         {
-            //if (romanToValue.contains(s[i]))
-            //{
-                if (i+1<s.length() && romanToValue(s[i])<romanToValue(s[i+1]))
-                {
-                    sum -= romanToValue(s[i]);
-                }
-                else
-                {
-                    sum += romanToValue(s[i]);
-                }
-            //}
+            if (i + 1 < s.length() && romanToValue(s[i]) < romanToValue(s[i + 1]))
+            {
+                // Subtractive combination detected, subtract current value.
+                sum -= romanToValue(s[i]);
+            }
+            else
+            {
+                // Regular addition of numeral value.
+                sum += romanToValue(s[i]);
+            }
         }
+
         return sum;
     }
-
 };
