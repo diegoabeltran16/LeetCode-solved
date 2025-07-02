ee09777d8a6b08ee1528684995114654ff296c69
Diego Alejandro Beltran <159483287+diegoabeltran16@users.noreply.github.com>
2025-07-01 21:49:14 -0500

    C# solution p14

---
+++ problems/14_Longest_Common_Prefix/csharp/longest_common_prefix_problem.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace LeetCodeProblems.LongestCommonPrefix
+{
+    public class ProblemDescription
+    {
+        // This class holds only the problem description as comments.
+        // See longest_common_prefix_solution.cs for implementation.
+    }
+}
